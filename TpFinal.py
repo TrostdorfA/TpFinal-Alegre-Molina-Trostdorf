eacfa7672cae6355fce1568cceaef84e2ec2b8fd
Matiteh <matias.molinada2@gmail.com>
2023-06-19 18:33:22 -0300

    Integracion con FastApi y SQlite3

---
+++ TpFinal.py
@@ -0,0 +1,186 @@
+import datetime
+from fastapi import FastAPI, Request, Form, Depends, Query
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError, HTTPException
+import sqlite3
+from sqlite3 import Error
+import subprocess
+import threading
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+conn = sqlite3.connect('tareas.db', check_same_thread=False)
+cursor = conn.cursor()
+
+class ConnectionPool:
+    def __init__(self, max_connections):
+        self.max_connections = max_connections
+        self.connections = []
+        self.lock = threading.Lock()
+
+    def get_connection(self):
+        with self.lock:
+            if len(self.connections) < self.max_connections:
+                connection = sqlite3.connect("tareas.db")
+                self.connections.append(connection)
+            else:
+                connection = self.connections.pop(0)
+            return connection
+
+    def release_connection(self, connection):
+        with self.lock:
+            self.connections.append(connection)
+
+pool = ConnectionPool(max_connections=10)
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    request.state.connection = pool.get_connection()
+    response = await call_next(request)
+    pool.release_connection(request.state.connection)
+    return response
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(status_code=400, content={"message": "Validation error"})
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(status_code=exc.status_code, content={"message": exc.detail})
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tareas (
+        uid INTEGER PRIMARY KEY,
+        titulo string,
+        descripcion string,
+        estado string,
+        creada datetime,
+        actualizada datetime
+    )
+''')
+conn.commit()
+
+class Tarea:
+    def __init__(self, uid, titulo, descripcion, estado, creada, actualizada):
+        self.uid = uid
+        self.titulo = titulo
+        self.descripcion = descripcion
+        self.estado = estado
+        self.creada = creada
+        self.actualizada = actualizada
+
+
+class AdminTarea:
+    @staticmethod
+    def agregar_tarea(titulo, descripcion, estado, creada, actualizada):
+        cursor.execute(
+            "INSERT INTO tareas (titulo, descripcion, estado, creada, actualizada) VALUES (?, ?, ?, ?, ?)",
+            (titulo, descripcion, estado, creada, actualizada)
+        )
+        conn.commit()
+        print("Tarea agregada con éxito!")
+
+    @staticmethod
+    def actualizar_estado(uid, estado):
+        cursor.execute(
+            "UPDATE tareas SET estado = ?, actualizada = ? WHERE uid = ?",
+            (estado, datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"), uid)
+        )
+        conn.commit()
+        print("Tarea actualizada con éxito!")
+
+    @staticmethod
+    def eliminar_tarea(uid):
+        cursor.execute("DELETE FROM tareas WHERE uid = ?", (uid,))
+        conn.commit()
+
+        
+    @staticmethod
+    def eliminar_todas_las_tareas():
+        cursor.execute("DELETE FROM tareas")
+        conn.commit()
+        print("Todas las tareas han sido eliminadas con éxito!")
+
+    @staticmethod
+    def __traer_tarea__(uid):
+        cursor.execute("SELECT * FROM tareas WHERE uid = ?", (uid,))
+        tarea_db = cursor.fetchone()
+        if tarea_db is not None:
+            tarea = Tarea(
+                tarea_db[0],  # uid
+                tarea_db[1],  # titulo
+                tarea_db[2],  # descripcion
+                tarea_db[3],  # estado
+                tarea_db[4],  # creada
+                tarea_db[5],  # actualizada
+            )
+            return tarea
+        else:
+            return None
+
+    @staticmethod
+    def __traer_todas_tareas__():
+        cursor.execute("SELECT * FROM tareas")
+        tareas_db = cursor.fetchall()
+        tareas = []
+        for tarea in tareas_db:
+            if tarea_db is not None:
+                tareas.append(Tarea(
+                    tarea[0],  # uid
+                    tarea[1],  # titulo
+                    tarea[2],  # descripcion
+                    tarea[3],  # estado
+                    tarea[4],  # creada
+                    tarea[5],  # actualizada
+                ))
+            else:
+                return None
+        return tareas
+   
+
+@app.get("/")
+def read_root(request: Request):
+    tareas = AdminTarea.__traer_todas_tareas__()
+    return templates.TemplateResponse("index.html", {"request": request, "tareas": tareas})
+
+
+@app.post("/agregar")
+def agregar_tarea(request: Request, tarea: dict):
+    titulo = tarea.get("titulo")
+    descripcion = tarea.get("descripcion")
+    estado = tarea.get("estado")
+    creada = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+    actualizada = creada
+    AdminTarea.agregar_tarea(titulo, descripcion, estado, creada, actualizada)
+
+
+@app.get("/eliminar/{uid}")
+def eliminar_tarea(uid: int):
+    AdminTarea.eliminar_tarea(uid)
+    return {"success": True}
+
+
+@app.get("/eliminar-todas")
+def eliminar_todas_tareas():
+    AdminTarea.eliminar_todas_las_tareas()
+    return {"success": True}
+
+
+@app.get("/actualizar/{uid}")
+def actualizar_estado(uid: int, estado: str= Query(...)):
+    AdminTarea.actualizar_estado(uid, estado)
+    return {"success": True}
+
+
+@app.get("/buscar/{uid}")
+def buscar_tarea(uid: int):
+    tarea = AdminTarea.__traer_tarea__(uid)
+    if tarea is not None:
+        return {"tarea": tarea}
+    else:
+        return {"tarea": None}
+ 
+def iniciar_servidor():
+    global proceso_servidor 
+    proceso_servidor= subprocess.Popen(["uvicorn", "TpFinal:app", "--reload"])
---
+++ interfaz.py
@@ -0,0 +1,259 @@
+from tkinter import Tk, Label, Entry, Button, Listbox, messagebox, Toplevel, Frame, simpledialog
+import TpFinal
+import datetime
+import requests
+import threading
+API_URL = "http://localhost:8000"
+
+class Interfaz(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Administrador de tareas")
+        self.geometry("800x700")
+        self.setup_ui()
+        self.load_tasks()
+        self.current_window = None  # Variable para almacenar la ventana abierta actualmente
+
+
+
+    def setup_ui(self):
+        self.button_frame = Frame(self)
+        self.button_frame.pack(side="right", padx=10, pady=10)
+
+        self.button_create = Button(self.button_frame, text="Crear", command=self.open_create_window)
+        self.button_create.pack(side="top", pady=5)
+
+        self.button_search = Button(self.button_frame, text="Buscar", command=self.open_search_window)
+        self.button_search.pack(side="top", pady=5)
+
+        self.button_update = Button(self.button_frame, text="Actualizar", command=self.update_task)
+        self.button_update.pack(side="top", pady=5)
+
+        self.button_delete = Button(self.button_frame, text="Eliminar", command=self.delete_task)
+        self.button_delete.pack(side="top", pady=5)
+
+        self.listbox_tasks = Listbox(self, width=100, height=30)
+        self.listbox_tasks.pack(padx=10, pady=10)
+        self.listbox_tasks.bind("<<ListboxSelect>>", self.update_description)
+
+        self.label_description = Label(self, text="", wraplength=700, justify="left")
+        self.label_description.pack(padx=10, pady=(0, 10))
+
+
+    def load_tasks(self):
+        self.listbox_tasks.delete(0, "end")
+        TpFinal.cursor.execute("SELECT * FROM tareas")
+        tareas = TpFinal.cursor.fetchall()
+        for tarea in tareas:
+            uid = tarea[0]
+            titulo = tarea[1]
+            estado = tarea[3]
+            creada = tarea[4]
+            actualizada = tarea[5]
+            task_text = f"{uid} | {titulo} | Estado: {estado} | Creación: {creada} | Actualización: {actualizada}"
+            self.listbox_tasks.insert("end", task_text)
+
+        if tareas:
+            self.update_description_by_uid(tareas[0][0])  # Actualizar la descripción con la primera tarea
+
+        self.update_description(None)
+
+    def update_task(self):
+        if self.current_window:
+            self.current_window.destroy()  # Cierra la ventana abierta actualmente
+
+        selected_index = self.listbox_tasks.curselection()
+        if selected_index:
+            tarea_text = self.listbox_tasks.get(selected_index[0])
+            tarea_uid = tarea_text.split(" | ")[0]
+            update_text = simpledialog.askstring("Actualizar Tarea", "Ingrese el nuevo estado:")
+            if update_text:
+                response = requests.get(f"{API_URL}/actualizar/{tarea_uid}", params={"estado": update_text})
+                if response.status_code == 200:
+                    self.load_tasks()
+                    messagebox.showinfo("Información", "Tarea actualizada con éxito!")
+                else:
+                    messagebox.showerror("Error", "Error al actualizar la tarea")
+        else:
+            messagebox.showinfo("Información", "Seleccione una tarea para actualizar.")
+
+    def delete_task(self):
+        if self.current_window:
+            self.current_window.destroy()  # Cierra la ventana abierta actualmente
+
+        selected_index = self.listbox_tasks.curselection()
+        if selected_index:
+            tarea_text = self.listbox_tasks.get(selected_index[0])
+            tarea_uid = tarea_text.split(" | ")[0]
+ 
+            self.current_window = Toplevel(self)
+            self.current_window.title("Eliminar tarea")
+            self.current_window.geometry("300x150")
+            self.current_window.resizable(False, False)
+            self.current_window.attributes("-topmost", True)
+
+            label_message = Label(self.current_window, text="¿Qué acción desea realizar?")
+            label_message.pack(pady=10)
+
+            button_delete_selected = Button(self.current_window, text="Eliminar tarea seleccionada", command=lambda: self.delete_selected_task(tarea_uid))
+            button_delete_selected.pack(pady=5)
+
+            button_delete_all = Button(self.current_window, text="Eliminar todas las tareas", command=self.delete_all_tasks)
+            button_delete_all.pack(pady=5)
+
+            # Centrar la ventana emergente en la pantalla
+            window_width = self.current_window.winfo_reqwidth()
+            window_height = self.current_window.winfo_reqheight()
+            position_right = int(self.current_window.winfo_screenwidth() / 2 - window_width / 2)
+            position_down = int(self.current_window.winfo_screenheight() / 2 - window_height / 2)
+            self.current_window.geometry("+{}+{}".format(position_right, position_down))
+        else:
+            messagebox.showinfo("Información", "Seleccione una tarea para eliminar.")
+
+    def delete_selected_task(self, uid):
+        response = requests.get(f"{API_URL}/eliminar/{uid}")
+        if response.status_code == 200:
+            self.load_tasks()
+            self.current_window.destroy()
+            messagebox.showinfo("Información", "Tarea eliminada con éxito!")
+        else:
+            messagebox.showerror("Error", "Error al eliminar la tarea")
+
+    def delete_all_tasks(self):
+        response = requests.get(f"{API_URL}/eliminar-todas")
+        if response.status_code == 200:
+            self.load_tasks()
+            self.current_window.destroy()
+            messagebox.showinfo("Información", "Todas las tareas han sido eliminadas.")
+        else:
+            messagebox.showerror("Error", "Error al eliminar todas las tareas")
+
+
+    def open_create_window(self):
+        if self.current_window:
+            self.current_window.destroy()  # Cierra la ventana abierta actualmente
+        create_window = Toplevel(self)
+        create_window.title("Crear tarea")
+        create_window.geometry("+%d+%d" % (self.winfo_screenwidth() / 2 - 200, self.winfo_screenheight() / 2 - 150))
+        create_window.grab_set()  # Bloquea la ventana principal hasta que se cierre la ventana emergente
+        self.current_window = create_window
+
+        label_title = Label(create_window, text="Título:")
+        label_title.pack()
+        entry_title = Entry(create_window)
+        entry_title.pack()
+
+        label_description = Label(create_window, text="Descripción:")
+        label_description.pack()
+        entry_description = Entry(create_window)
+        entry_description.pack()
+
+        label_status = Label(create_window, text="Estado:")
+        label_status.pack()
+        entry_status = Entry(create_window)
+        entry_status.pack()
+
+        button_save = Button(create_window, text="Guardar", command=lambda: self.save_task(entry_title.get(), entry_description.get(), entry_status.get(), create_window))
+        button_save.pack(pady=5)
+
+        button_cancel = Button(create_window, text="Cancelar", command=create_window.destroy)
+        button_cancel.pack(pady=5)
+
+    def save_task(self, titulo, descripcion, estado, create_window):
+        payload = {
+            "titulo": str(titulo),
+            "descripcion": str(descripcion),
+            "estado": str(estado),
+        }
+
+        # Enviar la solicitud POST a la API
+        response = requests.post(f"{API_URL}/agregar", json=payload)
+        self.load_tasks()
+        create_window.destroy()
+        messagebox.showinfo("Información", "Tarea creada con éxito!")
+
+
+    def open_search_window(self):
+        if self.current_window:
+            self.current_window.destroy()  # Cierra la ventana abierta actualmente
+        search_window = Toplevel(self)
+        search_window.title("Buscar tarea")
+        search_window.geometry("+%d+%d" % (self.winfo_screenwidth() / 2 - 200, self.winfo_screenheight() / 2 - 150))
+        search_window.grab_set()  # Bloquea la ventana principal hasta que se cierre la ventana emergente
+        self.current_window = search_window
+
+        label_uid = Label(search_window, text="UID:")
+        label_uid.pack()
+        entry_uid = Entry(search_window)
+        entry_uid.pack()
+
+        button_search = Button(search_window, text="Buscar", command=lambda: self.search_task_by_uid(entry_uid.get(), search_window))
+        button_search.pack(pady=5)
+
+        button_cancel = Button(search_window, text="Cancelar", command=self.cancel_search)
+        button_cancel.pack(pady=5)
+
+    def search_task_by_uid(self, uid, search_window):
+        if uid.isdigit():
+            uid = int(uid)
+            response = requests.get(f"{API_URL}/buscar/{uid}")
+            if response.status_code == 200:
+                data = response.json()
+                tarea = data.get("tarea")
+                if tarea:
+                    index = uid - 1
+                    self.listbox_tasks.selection_clear(0, "end")
+                    self.listbox_tasks.selection_set(index)
+                    self.listbox_tasks.see(index)
+                    self.update_description_by_uid(uid)  # Actualizar la descripción
+                    messagebox.showinfo("Información", "Tarea encontrada: {}".format(tarea["titulo"]))
+                else:
+                    self.label_description.config(text="")  # Limpiar la descripción si no se encuentra la tarea
+                    messagebox.showinfo("Información", "No se encontró ninguna tarea con el UID: {}".format(uid))
+            else:
+                messagebox.showerror("Error", "Error al buscar la tarea")
+        else:
+            messagebox.showerror("Error", "Ingrese un UID válido")
+
+        search_window.destroy()
+
+    def update_description_by_uid(self, uid):
+        tarea = TpFinal.AdminTarea.__traer_tarea__(uid)
+        if tarea:
+            descripcion = f"UID: {tarea.uid}\nTítulo: {tarea.titulo}\nDescripción: {tarea.descripcion}\nEstado: {tarea.estado}\nCreada: {tarea.creada}\nActualizada: {tarea.actualizada}"
+            self.label_description.config(text=descripcion)
+        else:
+            self.label_description.config(text="")
+
+    def update_description(self, event):
+        # Obtener el índice de la tarea seleccionada en la Listbox
+        index = self.listbox_tasks.curselection()
+
+        if index:
+            # Obtener la tarea seleccionada
+            tarea_text = self.listbox_tasks.get(index)
+            tarea_uid = tarea_text.split(" | ")[0]
+            tarea = TpFinal.AdminTarea.__traer_tarea__(int(tarea_uid))
+
+            # Actualizar la etiqueta de descripción con los datos de la tarea
+            descripcion = f"UID: {tarea.uid}\nTítulo: {tarea.titulo}\nDescripción: {tarea.descripcion}\nEstado: {tarea.estado}\nCreada: {tarea.creada}\nActualizada: {tarea.actualizada}"
+            self.label_description.config(text=descripcion)
+        else:
+            self.label_description.config(text="")
+
+    def cancel_search(self):
+        if self.current_window:
+            self.current_window.destroy()
+
+def cerrar_aplicacion():
+# Detener el servidor de la API
+    if TpFinal.proceso_servidor:
+        TpFinal.proceso_servidor.terminate()
+    app.quit()
+
+if __name__ == "__main__":
+    thread_servidor= threading.Thread(target=TpFinal.iniciar_servidor)
+    thread_servidor.start()
+    app = Interfaz()
+    app.protocol("WM_DELETE_WINDOW", cerrar_aplicacion)
+    app.mainloop()
